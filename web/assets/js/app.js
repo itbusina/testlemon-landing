5a5f7eeb3f945c20cb7a89a18bde691d205ddadb
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-20 03:32:38 +0200

    moved contact to templates

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,14 +1,16 @@
 import { _variables } from './variables.js';
 import { _main } from './main.js';
 import { _gdpr } from './gdpr.js';
-import { cookieConsentTemplate, scrollTopTemplate, footerTemplate } from './templates.js';
+import { cookieConsentTemplate, scrollTopTemplate, footerTemplate, contactTemplate } from './templates.js';
 
 let _vars = _variables();
 
 let gdpr = document.getElementById('gdpr');
 let scroll = document.getElementById('scroll');
 let footer = document.getElementById('footer');
+let contact = document.getElementById('contact');
 
+contact.insertAdjacentHTML('beforeend', contactTemplate(_vars.companyAddress, _vars.companyEmail));
 footer.insertAdjacentHTML('beforeend', footerTemplate());
 scroll.insertAdjacentHTML('beforeend', scrollTopTemplate());
 gdpr.insertAdjacentHTML('beforeend', cookieConsentTemplate(_vars.privacyUrl));
--- web/assets/js/variables.js
+++ web/assets/js/variables.js
@@ -1,6 +1,8 @@
 export function _variables() {
     return {
         privacyUrl: "https://docs.apibee.app/privacy-policy",
-        GATrackingID: "G-XXXXXX"
+        GATrackingID: "G-XXXXXX",
+        companyAddress: "Lakowa 60b/11, 1th Floor, 80-769, Gdansk",
+        companyEmail: "contact@itbusina.com"
     };
 }
