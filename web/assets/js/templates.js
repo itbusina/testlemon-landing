746fc9bc64e0950b6bccab7a074b2624903a1250
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-20 04:33:22 +0200

    Fixed gdpr widget.

--- web/assets/js/templates.js
+++ web/assets/js/templates.js
@@ -5,7 +5,7 @@ export function insertTemplate (selector, position, html) {
 
 export function cookieConsentTemplate(privacyUrl) {
   return `
-    <div id="gdpr-banner" class="text-xs fixed bottom-8 left-4 z-[999] inline-flex items-center gap-[10px] rounded-lg bg-white px-[14px] py-2 shadow-2 dark:bg-dark-2 sm:left-9">
+    <div id="gdpr-banner" style="display: none;" class="text-xs fixed bottom-8 left-4 z-[999] inline-flex items-center gap-[10px] rounded-lg bg-white px-[14px] py-2 shadow-2 dark:bg-dark-2 sm:left-9">
         <span class="font-medium text-dark-3 dark:text-dark-6">This website uses cookies.<br /><a href="${privacyUrl}"
             style="text-decoration: underline;">Learn more</a>
         </span>
