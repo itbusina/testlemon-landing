9b2417f89e6f45bc92006bf8c4425939b66ad97f
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-20 03:09:41 +0200

    Moved scrollTopTemplate to templates

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,7 +1,10 @@
 import { _variables } from './variables.js';
-import { cookieConsentTemplate } from './templates.js';
+import { cookieConsentTemplate, scrollTopTemplate } from './templates.js';
 
 let _vars = _variables();
-let gdpr = document.getElementById('gdpr');
 
+let gdpr = document.getElementById('gdpr');
+let scrollToTop = document.getElementById('scrollToTop');
+
+scrollToTop.insertAdjacentHTML('beforeend', scrollTopTemplate());
 gdpr.insertAdjacentHTML('beforeend', cookieConsentTemplate(_vars.privacyUrl));
--- web/assets/js/templates.js
+++ web/assets/js/templates.js
@@ -11,3 +11,11 @@ export function cookieConsentTemplate(privacyUrl) {
         </div>
     `;
 }
+
+export function scrollTopTemplate() {
+    return `
+        <a href="javascript:void(0)" class="back-to-top fixed bottom-8 left-auto right-8 z-[999] hidden h-10 w-10 items-center justify-center rounded-md bg-primary text-white shadow-md transition duration-300 ease-in-out hover:bg-dark">
+            <span class="mt-[6px] h-3 w-3 rotate-45 border-l border-t border-white"></span>
+        </a>
+    `;
+}
