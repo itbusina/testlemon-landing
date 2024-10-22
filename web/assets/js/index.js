999dbfe01393653279aa9ed9380099b125c84c7a
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-22 23:03:14 +0200

    update index page

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -19,14 +19,15 @@ import {
 } from './templates.js';
 
 let _vars = _variables();
+let config = window.config;
 
 insertTemplate('nav', 'beforeend', navTemplate("lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70", "text-white dark:hidden", "text-white", "hidden", "header-logo", "bg-white bg-opacity-20 hover:bg-opacity-100 hover:text-dark"));
 insertTemplate('home', 'beforeend', homeTemplate());
-insertTemplate('features', 'beforeend', featuresTemplate(window._featuresHeanline, window._featuresMessage, window._features));
-insertTemplate('about', 'beforeend', aboutTemplate());
-insertTemplate('cta', 'beforeend', ctaTemplate());
+insertTemplate('features', 'beforeend', featuresTemplate(config.featuresHeanline, config.featuresMessage, config.features));
+insertTemplate('about', 'beforeend', aboutTemplate(config.aboutTitle, config.aboutText));
+insertTemplate('cta', 'beforeend', ctaTemplate(config.ctaHeadline, config.ctaMessage, config.ctaButtonText));
 insertTemplate('pricing', 'beforeend', pricingTemplate());
-insertTemplate('faq', 'beforeend', faqTemplate());
+insertTemplate('faq', 'beforeend', faqTemplate(config.faqs));
 insertTemplate('blog', 'beforeend', blogTemplate());
 insertTemplate('contact', 'beforeend', contactTemplate(_vars.companyAddress, _vars.companyEmail));
 insertTemplate('footer', 'beforeend', footerTemplate());
