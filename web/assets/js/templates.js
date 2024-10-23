85f7cf3a89df92c45b36f20f1cd0c232207e5c7e
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-24 00:05:30 +0200

    added ai augmented testing page

--- web/assets/js/templates.js
+++ web/assets/js/templates.js
@@ -160,7 +160,7 @@ export function addFooter(selector = 'footer') {
                 </a>
               </li>
               <li>
-                <a href="javascript:void(0)" class="mb-3 inline-block text-base text-gray-7 hover:text-primary">
+                <a href="ai-augmented-testing.html" class="mb-3 inline-block text-base text-gray-7 hover:text-primary">
                   AI-augmented testing
                 </a>
               </li>
@@ -1486,7 +1486,7 @@ export function addNav(selector = 'nav', menuItemCssClass, themeSwitcherCssClass
                       class="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary">
                       Tests automation
                     </a>
-                    <a href="#"
+                    <a href="ai-augmented-testing.html"
                       class="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary">
                       AI-augmented testing
                     </a>
