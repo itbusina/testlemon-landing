01782dec8f4a3f8128b4443d134d0fee73679816
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-24 17:14:04 +0200

    updated a link to examples

--- web/assets/js/variables.js
+++ web/assets/js/variables.js
@@ -4,7 +4,7 @@ export function _variables() {
         termsUrl: "https://docs.testlemon.com/terms-of-use/",
         signInUrl: "https://app.testlemon.com",
         signUpUrl: "https://app.testlemon.com/accounts/account/login",
-        githubExamplesUrl: "https://github.com/itbusina/testlemon-docs/tree/main/src/examples",
+        githubExamplesUrl: "https://github.com/itbusina/testlemon-docs/tree/main/examples",
         GATrackingID: "G-11N6YDHGVM",
         companyAddress: "Lakowa 60b/11, 1th Floor, 80-769, Gdansk",
         companyEmail: "contact@testlemon.com"
