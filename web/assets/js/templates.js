906c2becff3f02c3f754a490cb060f22973c500d
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-24 14:51:05 +0200

    updated social icons

--- web/assets/js/templates.js
+++ web/assets/js/templates.js
@@ -117,39 +117,20 @@ export function addFooter(selector = 'footer') {
               <img style="height: 50px;" src="assets/images/logo/logo-white.svg" alt="logo" class="max-w-full" />
             </a>
             <p class="mb-8 max-w-[270px] text-base text-gray-7">
-              We create new monitoring and testing experiences.
+              Creating new testing experience.
             </p>
             <div class="-mx-3 flex items-center">
-              <a href="https://www.linkedin.com/company/itbusina/" class="px-3 text-gray-7 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="fill-current" viewBox="0 0 256 256">
-                  <path
-                    d="M216,24H40A16,16,0,0,0,24,40V216a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V40A16,16,0,0,0,216,24Zm0,192H40V40H216V216ZM96,112v64a8,8,0,0,1-16,0V112a8,8,0,0,1,16,0Zm88,28v36a8,8,0,0,1-16,0V140a20,20,0,0,0-40,0v36a8,8,0,0,1-16,0V112a8,8,0,0,1,15.79-1.78A36,36,0,0,1,184,140ZM100,84A12,12,0,1,1,88,72,12,12,0,0,1,100,84Z">
-                  </path>
-                </svg>
-              </a>
-              <a href="https://www.facebook.com/itbusina/" class="px-3 text-gray-7 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="fill-current" viewBox="0 0 256 256"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm8,191.63V152h24a8,8,0,0,0,0-16H136V112a16,16,0,0,1,16-16h16a8,8,0,0,0,0-16H152a32,32,0,0,0-32,32v24H96a8,8,0,0,0,0,16h24v63.63a88,88,0,1,1,16,0Z"></path></svg>
-              </a>
               <a href="https://x.com/TestLemonCo" class="px-3 text-gray-7 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="fill-current" viewBox="0 0 256 256">
-                  <path
-                    d="M214.75,211.71l-62.6-98.38,61.77-67.95a8,8,0,0,0-11.84-10.76L143.24,99.34,102.75,35.71A8,8,0,0,0,96,32H48a8,8,0,0,0-6.75,12.3l62.6,98.37-61.77,68a8,8,0,1,0,11.84,10.76l58.84-64.72,40.49,63.63A8,8,0,0,0,160,224h48a8,8,0,0,0,6.75-12.29ZM164.39,208,62.57,48h29L193.43,208Z">
-                  </path>
-                </svg>
+                <i class="ph ph-x-logo"></i>  
+              </a>
+              <a href="https://www.linkedin.com/company/itbusina/" class="px-3 text-gray-7 hover:text-white">
+                <i class="ph ph-linkedin-logo"></i>
               </a>
               <a href="https://join.slack.com/t/itbusina/shared_invite/zt-2rue98n0h-HcfUjUgOiwz3lqXLVnBm0Q/" class="px-3 text-gray-7 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="fill-current" viewBox="0 0 256 256">
-                  <path
-                    d="M221.13,128A32,32,0,0,0,184,76.31V56a32,32,0,0,0-56-21.13A32,32,0,0,0,76.31,72H56a32,32,0,0,0-21.13,56A32,32,0,0,0,72,179.69V200a32,32,0,0,0,56,21.13A32,32,0,0,0,179.69,184H200a32,32,0,0,0,21.13-56ZM72,152a16,16,0,1,1-16-16H72Zm48,48a16,16,0,0,1-32,0V152a16,16,0,0,1,16-16h16Zm0-80H56a16,16,0,0,1,0-32h48a16,16,0,0,1,16,16Zm0-48H104a16,16,0,1,1,16-16Zm16-16a16,16,0,0,1,32,0v48a16,16,0,0,1-16,16H136Zm16,160a16,16,0,0,1-16-16V184h16a16,16,0,0,1,0,32Zm48-48H152a16,16,0,0,1-16-16V136h64a16,16,0,0,1,0,32Zm0-48H184V104a16,16,0,1,1,16,16Z">
-                  </path>
-                </svg>
+                <i class="ph ph-slack-logo"></i>
               </a>
               <a href="https://www.youtube.com/@itbusina/" class="px-3 text-gray-7 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="fill-current" viewBox="0 0 256 256">
-                  <path
-                    d="M164.44,121.34l-48-32A8,8,0,0,0,104,96v64a8,8,0,0,0,12.44,6.66l48-32a8,8,0,0,0,0-13.32ZM120,145.05V111l25.58,17ZM234.33,69.52a24,24,0,0,0-14.49-16.4C185.56,39.88,131,40,128,40s-57.56-.12-91.84,13.12a24,24,0,0,0-14.49,16.4C19.08,79.5,16,97.74,16,128s3.08,48.5,5.67,58.48a24,24,0,0,0,14.49,16.41C69,215.56,120.4,216,127.34,216h1.32c6.94,0,58.37-.44,91.18-13.11a24,24,0,0,0,14.49-16.41c2.59-10,5.67-28.22,5.67-58.48S236.92,79.5,234.33,69.52Zm-15.49,113a8,8,0,0,1-4.77,5.49c-31.65,12.22-85.48,12-86,12H128c-.54,0-54.33.2-86-12a8,8,0,0,1-4.77-5.49C34.8,173.39,32,156.57,32,128s2.8-45.39,5.16-54.47A8,8,0,0,1,41.93,68c30.52-11.79,81.66-12,85.85-12h.27c.54,0,54.38-.18,86,12a8,8,0,0,1,4.77,5.49C221.2,82.61,224,99.43,224,128S221.2,173.39,218.84,182.47Z">
-                  </path>
-                </svg>
+                <i class="ph ph-youtube-logo"></i>
               </a>
             </div>
           </div>
