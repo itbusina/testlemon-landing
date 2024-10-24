1bec25021039c9d889da55af09eba0cfcc6c8373
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-24 10:50:09 +0200

    Updated a link to testlemon twitter

--- web/assets/js/templates.js
+++ web/assets/js/templates.js
@@ -130,7 +130,7 @@ export function addFooter(selector = 'footer') {
               <a href="https://www.facebook.com/itbusina/" class="px-3 text-gray-7 hover:text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="fill-current" viewBox="0 0 256 256"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm8,191.63V152h24a8,8,0,0,0,0-16H136V112a16,16,0,0,1,16-16h16a8,8,0,0,0,0-16H152a32,32,0,0,0-32,32v24H96a8,8,0,0,0,0,16h24v63.63a88,88,0,1,1,16,0Z"></path></svg>
               </a>
-              <a href="https://x.com/itbusina/" class="px-3 text-gray-7 hover:text-white">
+              <a href="https://x.com/TestLemonCo" class="px-3 text-gray-7 hover:text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="fill-current" viewBox="0 0 256 256">
                   <path
                     d="M214.75,211.71l-62.6-98.38,61.77-67.95a8,8,0,0,0-11.84-10.76L143.24,99.34,102.75,35.71A8,8,0,0,0,96,32H48a8,8,0,0,0-6.75,12.3l62.6,98.37-61.77,68a8,8,0,1,0,11.84,10.76l58.84-64.72,40.49,63.63A8,8,0,0,0,160,224h48a8,8,0,0,0,6.75-12.29ZM164.39,208,62.57,48h29L193.43,208Z">
