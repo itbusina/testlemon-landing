895b2466fd006469c9c652e4c2dc7146c6240c53
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-24 03:49:52 +0200

    updated cooke concent

--- web/assets/js/templates.js
+++ web/assets/js/templates.js
@@ -9,8 +9,8 @@ function insertTemplate(selector, html) {
 export function addCookieConsent(selector = 'gdpr', trackingID, privacyUrl) {
   let html = `
     <div id="gdpr-banner" style="display: none;" class="text-xs fixed bottom-8 left-4 z-[999] inline-flex items-center gap-[10px] rounded-lg bg-white px-[14px] py-2 shadow-2 dark:bg-dark-2 sm:left-9">
-        <span class="font-medium text-dark-3 dark:text-dark-6">This website uses cookies.<br /><a href="${privacyUrl}"
-            style="text-decoration: underline;">Learn more</a>
+        <span class="font-medium text-dark-3 dark:text-dark-6">This website uses cookies.
+          <a href="${privacyUrl}" style="text-decoration: underline;"> Learn more</a>
         </span>
         <span class="block h-4 w-px bg-stroke dark:bg-dark-3"></span>
         <button class="font-medium text-primary dark:text-white" onclick="handleGDPRConsent(true)">Accept</button>
