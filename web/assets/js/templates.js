99c9a3c94332bcacd830766e5db945d46944f5a3
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-24 02:39:24 +0200

    update the link on scroll to prevent going to comins soon page

--- web/assets/js/templates.js
+++ web/assets/js/templates.js
@@ -62,7 +62,7 @@ export function addCookieConsent(selector = 'gdpr', trackingID, privacyUrl) {
 
 export function addScrollTop(selector = 'scroll') {
   let html = `
-    <a href="javascript:void(0)" class="back-to-top fixed bottom-8 left-auto right-8 z-[999] hidden h-10 w-10 items-center justify-center rounded-md bg-primary text-white shadow-md transition duration-300 ease-in-out hover:bg-dark">
+    <a href="#top" class="back-to-top fixed bottom-8 left-auto right-8 z-[999] hidden h-10 w-10 items-center justify-center rounded-md bg-primary text-white shadow-md transition duration-300 ease-in-out hover:bg-dark">
         <span class="mt-[6px] h-3 w-3 rotate-45 border-l border-t border-white"></span>
     </a>
     `;
