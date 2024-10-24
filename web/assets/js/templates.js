1ab48b653f8610fcad5efbb0ff1f88dfbae4c5da
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-24 10:31:08 +0200

    Fixed variables

--- web/assets/js/templates.js
+++ web/assets/js/templates.js
@@ -1,4 +1,4 @@
-import { _variables as vars } from './page.js';
+import { _variables as vars } from './_variables.js';
 
 function insertTemplate(selector, html) {
   let element = document.getElementById(selector);
