4de0e4d931372dc66ea6f3a76a8b6c59cb67c34f
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-10-29 13:55:09 +0100

    Fixed link to sign up page

--- blob/web/assets/js/variables.js
+++ blob/web/assets/js/variables.js
@@ -3,7 +3,7 @@ export function _variables() {
         privacyUrl: "https://docs.testlemon.com/privacy-policy/",
         termsUrl: "https://docs.testlemon.com/terms-of-use/",
         signInUrl: "https://app.testlemon.com",
-        signUpUrl: "https://app.testlemon.com/accounts/account/login",
+        signUpUrl: "https://app.testlemon.com/account/login",
         githubExamplesUrl: "https://github.com/itbusina/testlemon-docs/tree/main/examples",
         GATrackingID: "G-11N6YDHGVM",
         companyAddress: "Lakowa 60b/11, 1th Floor, 80-769, Gdansk",
