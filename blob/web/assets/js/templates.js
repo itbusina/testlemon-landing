c9b4ddd513eee6c4d05a6eb32f64418cdb44f2e4
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2024-12-07 16:57:13 +0100

    added a page about jria xray integration. Updated sitemap

--- blob/web/assets/js/templates.js
+++ blob/web/assets/js/templates.js
@@ -244,6 +244,16 @@ export function addFooter(selector = 'footer') {
             </h4>
             <ul>
               <li>
+                <a href="https://docs.testlemon.com/" target="_blank" class="mb-3 inline-block text-base text-gray-7 hover:text-primary">
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0)" class="mb-3 inline-block text-base text-gray-7 hover:text-primary">
+                  Integrations
+                </a>
+              </li>
+              <li>
                 <a href="javascript:void(0)" class="mb-3 inline-block text-base text-gray-7 hover:text-primary">
                   Live Demo
                 </a>
@@ -266,11 +276,6 @@ export function addFooter(selector = 'footer') {
               <li>
                 <a href="javascript:void(0)" class="mb-3 inline-block text-base text-gray-7 hover:text-primary">
                   Comparisons
-                </a>
-              </li>
-              <li>
-                <a href="javascript:void(0)" class="mb-3 inline-block text-base text-gray-7 hover:text-primary">
-                  Integrations
                 </a>
               </li>
               <li>
@@ -347,8 +352,7 @@ export function addFooter(selector = 'footer') {
             <div class="my-1 flex justify-center md:justify-end">
               <p class="text-base text-gray-7">
                 Developed by
-                <a href="https://itbusina.com" rel="nofollow noopner" target="_blank"
-                  class="text-gray-1 hover:underline">
+                <a href="https://itbusina.com" rel="nofollow noopner" target="_blank" class="text-gray-1 hover:underline">
                   itbusina.com
                 </a>
               </p>
@@ -1080,7 +1084,7 @@ export function addPricing(selector = 'pricing') {
   insertTemplate(selector, html);
 }
 
-export function addCta(selector = 'cta', headline = "Headline", message = "message", buttonText) {
+export function addCta(selector = 'cta', headline = "Headline", message = "message", buttonText, buttonLink = vars.signInUrl) {
   let html = `
   <div class="container mx-auto">
       <div class="relative overflow-hidden">
@@ -1096,8 +1100,7 @@ export function addCta(selector = 'cta', headline = "Headline", message = "messa
               <p class="mx-auto mb-6 max-w-[515px] text-base leading-[1.5] text-white">
                 No credit card required!
               </p>
-              <a href="${vars.signInUrl}"
-                class="inline-block rounded-md border border-transparent bg-secondary px-7 py-3 text-base font-medium text-dark transition hover:bg-[#0BB489]">
+              <a href="${buttonLink}" target="_blank" class="inline-block rounded-md border border-transparent bg-secondary px-7 py-3 text-base font-medium text-dark transition hover:bg-[#0BB489]">
                 ${buttonText}
               </a>
             </div>
@@ -1535,11 +1538,39 @@ export function addNav(selector = 'nav', menuItemCssClass, themeSwitcherCssClass
                     </a>
                   </div>
                 </li>
-                <li class="group relative">
-                  <a href="https://docs.testlemon.com"
-                    class="flex ud-menu-scroll mx-8 py-2 text-base font-medium text-dark dark:text-white group-hover:text-primary lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 xl:ml-10 ${menuItemCssClass}">
-                    Documentation
+                <li class="submenu-item group relative">
+                  <a href="#monitoring" class="flex mx-8 py-2 text-base font-medium text-dark dark:text-white group-hover:text-primary lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 xl:ml-10 ${menuItemCssClass}">
+                    Integrations
+
+                    <svg class="ml-2 fill-current" width="16" height="20" viewBox="0 0 16 20" fill="none"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M7.99999 14.9C7.84999 14.9 7.72499 14.85 7.59999 14.75L1.84999 9.10005C1.62499 8.87505 1.62499 8.52505 1.84999 8.30005C2.07499 8.07505 2.42499 8.07505 2.64999 8.30005L7.99999 13.525L13.35 8.25005C13.575 8.02505 13.925 8.02505 14.15 8.25005C14.375 8.47505 14.375 8.82505 14.15 9.05005L8.39999 14.7C8.27499 14.825 8.14999 14.9 7.99999 14.9Z" />
+                    </svg>
                   </a>
+                  <div
+                    class="submenu relative left-0 top-full hidden w-[250px] rounded-sm bg-white p-4 transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark-2 lg:invisible lg:absolute lg:top-[110%] lg:block lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full">
+                    <a href="jira-xray-integration.html"
+                      class="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary">
+                      Jira Xray integration
+                    </a>
+                    <a href="javascript:void(0)"
+                      class="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary">
+                      GitHub integration
+                    </a>
+                    <a href="javascript:void(0)"
+                      class="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary">
+                      OpenAI integration
+                    </a>
+                    <a href="javascript:void(0)"
+                      class="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary">
+                      Slack integration
+                    </a>
+                    <a href="javascript:void(0)"
+                      class="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary">
+                      Webhook integration
+                    </a>
+                  </div>
                 </li>
                 <li class="group relative">
                   <a href="index.html#pricing"
@@ -1799,7 +1830,7 @@ export function addNav(selector = 'nav', menuItemCssClass, themeSwitcherCssClass
   window.document.addEventListener("scroll", onScroll);
 }
 
-export function addBanner(selector = 'banner', headline = "Headline", message = "message", buttonText = "Start for FREE") {
+export function addBanner(selector = 'banner', headline = "Headline", message = "message", buttonText = "Start for FREE", bannerLink = vars.signInUrl) {
   let html = `
   <div class="absolute bottom-0 left-0 w-full h-px bg-gradient-to-r from-stroke/0 via-stroke to-stroke/0 dark:via-dark-3"></div>
       <div class="container">
@@ -1814,7 +1845,7 @@ export function addBanner(selector = 'banner', headline = "Headline", message = 
               </p>
               <ul class="mb-10 flex flex-wrap items-center justify-center gap-5">
               <li>
-                <a href="${vars.signInUrl}" class="inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2 hover:text-body-color">
+                <a href="${bannerLink}" target="_blank" class="inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2 hover:text-body-color">
                   ${buttonText}
                 </a>
               </li>
