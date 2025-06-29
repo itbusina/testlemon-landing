770bc95b9fe708f574184bcf9da02361d5baa51c
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2025-06-29 15:42:31 +0200

    updated the links to stripe

--- blob/web/assets/js/variables.js
+++ blob/web/assets/js/variables.js
@@ -9,7 +9,9 @@ export function _variables() {
         GATrackingID: "G-11N6YDHGVM",
         companyAddress: "Lakowa 60b/11, 1th Floor, 80-769, Gdansk, Poland",
         companyEmail: "contact@testlemon.com",
-        monthlyPriceUrl: "https://buy.stripe.com/test_cNiaEW4WL2GR4M7cDUg3600",
-        yearlyPriceUrl: "https://buy.stripe.com/test_eVq6oG1KzgxHcezgUag3602",
+        // monthlyPriceUrl: "https://buy.stripe.com/test_cNiaEW4WL2GR4M7cDUg3600",
+        // yearlyPriceUrl: "https://buy.stripe.com/test_eVq6oG1KzgxHcezgUag3602",
+        monthlyPriceUrl: "https://buy.stripe.com/test_9B65kCcpd4OZ5Qb1Zgg3603",
+        yearlyPriceUrl: "https://buy.stripe.com/test_3cI7sKexldlv0vR1Zgg3604",
     };
 }
